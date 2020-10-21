d40cc7e799feee9570a9c14a754a621e31e20206
Dangerpwnd <reric2012@gmail.com>
2020-10-21 15:52:33 -0500

    Removed Id, added setter

--- QxAPI/Models/Job.cs
+++ QxAPI/Models/Job.cs
@@ -1,7 +1,8 @@
+using System;
+
 public class Job {
-    public long Job_Id { get; }    
-    public DateTime Job_Date { get; }
-    public string Job_Site { get; }
-    public string Job_Stage { get; }
-    public string Job_City { get; }
+    public DateTime Job_Date { get; set; }
+    public string Job_Site { get; set; }
+    public string Job_Stage { get; set; }
+    public string Job_City { get; set; }
 }
