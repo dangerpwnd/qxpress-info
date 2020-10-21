c33b446ce6c6fa028a35822ccb355e50169898bd
Dangerpwnd <reric2012@gmail.com>
2020-10-21 15:53:01 -0500

    Added setter; Using OnModelCreating for view

--- QxAPI/Models/JobContext.cs
+++ QxAPI/Models/JobContext.cs
@@ -6,6 +6,17 @@ namespace QxAPI.Models {
         public JobContext(DbContextOptions<JobContext> options): base(options){
             ChangeTracker.QueryTrackingBehavior=QueryTrackingBehavior.NoTracking;
         }
-        public DbSet<Job> Jobs { get; }
+        public DbSet<Job> Jobs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<Job>(eb => {
+                eb.HasNoKey();
+                eb.ToView("qryRpt_AllSchedItems1");
+                eb.Property(v => v.Job_Date).HasColumnName("Job_Date");
+                eb.Property(v => v.Job_Site).HasColumnName("JobSiteFileName");
+                eb.Property(v => v.Job_Stage).HasColumnName("Job_Description");
+                eb.Property(v => v.Job_City).HasColumnName("JobSiteLine2");
+            });
+        }
     }
 }
