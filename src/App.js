e6b413e483a052e885bba51dda11522f88e49769
Dangerpwnd <reric2012@gmail.com>
2020-11-10 16:39:45 -0600

    Moving QxList to QxForm

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "./App.css";
 import QxForm from "./Components/QxSearch/QxForm/QxForm";
-import QxList from "./Components/QxSearch/QxList/QxList";
-
 
 const App = props => {
   return (
     <>
       <h1>Qxpress Job Reports</h1>
       <QxForm />
-      <QxList />
     </>
   );
 };
