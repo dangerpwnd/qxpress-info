11690f80506f52f7c61a92c42941f90ad55dbaf9
Dangerpwnd <reric2012@gmail.com>
2020-11-18 15:39:01 -0600

    Added react-error-boundary; Wrapped QxForm component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./App.css";
 import QxForm from "./Components/QxSearch/QxForm/QxForm";
+import {ErrorBoundary} from 'react-error-boundary';
+
+const ErrorFallback = ({error, resetErrorBoundary}) => {
+  return (
+    <div>
+      <p>Scream at Eric. He broke it.</p>
+      <p>{error.message}</p>
+      <button onClick={resetErrorBoundary}>Try Again</button>
+    </div>
+  )
+}
 
 const App = props => {
   return (
     <>
       <h1>Qxpress Job Reports</h1>
-      <QxForm />
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}  
+      >
+        <QxForm />
+      </ErrorBoundary>
     </>
   );
 };
