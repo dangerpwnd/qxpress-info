36c71c25212ff1b647d21b63da82ad4ad93de456
Dangerpwnd <reric2012@gmail.com>
2020-11-25 13:42:46 -0600

    Added init var outside qxlist; Updated useEff to use submit prop

--- src/Components/QxSearch/QxList/QxList.js
+++ src/Components/QxSearch/QxList/QxList.js
@@ -4,33 +4,28 @@ import ReactPaginate from 'react-paginate';
 
 import "./QxList.css";
 
+const initialPaginate = {
+  offset: 0,
+  perPage: 10,
+  currentPage: 0,
+  pageCount: 0
+}
+
 const QxList = (props) => {
 
   //Paginate State
-  const [paginate, setPaginate] = useState({
-      offset: 0,
-      perPage: 10,
-      currentPage: 0,
-      pageCount: 0
-  })
-
-  // Table headers
-  const headers = [
-    { field: "job_Date", label: "Date" },
-    { field: "job_Site", label: "Address" },
-    { field: "job_City", label: "City" },
-    { field: "job_Stage", label: "Stage" },
-  ];
+  const [paginate, setPaginate] = useState(initialPaginate);
 
   //Job Data
-  const jobData = props.formData;
-  const sliceData = jobData.slice(paginate.offset, paginate.offset + paginate.perPage);
+  const {formData, hasSubmit} = props;
 
   useEffect(() => {
-    setPaginate({
-      ...paginate,
-      pageCount: Math.ceil(jobData.length / paginate.perPage)
-    })}, [jobData]);
+    if(hasSubmit){
+      let object = {...initialPaginate}
+      object.pageCount = Math.ceil(formData.length / object.perPage)
+      setPaginate(object);
+    }
+    }, [hasSubmit, formData]);
   
 
   //Setting Page/Per Page
@@ -45,6 +40,16 @@ const QxList = (props) => {
     });
 
   }
+
+  const sliceData = formData.slice(paginate.offset, paginate.offset + paginate.perPage);
+
+  // Table headers
+  const headers = [
+    { field: "job_Date", label: "Date" },
+    { field: "job_Site", label: "Address" },
+    { field: "job_City", label: "City" },
+    { field: "job_Stage", label: "Stage" },
+  ];
 
   return (
     <div className="FlexList">
