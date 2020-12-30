b97cfbc77cedc8d4798c57e4527c95f27afe08c1
Dangerpwnd <reric2012@gmail.com>
2020-12-30 12:45:45 -0600

    Added Builder Prop; Used custom view for query

--- QxAPI/Models/Job.cs
+++ QxAPI/Models/Job.cs
@@ -1,8 +1,12 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class Job {
     public DateTime Job_Date { get; set; }
-    public string Job_Site { get; set; }
-    public string Job_Stage { get; set; }
+    public string Job_Builder {get; set;}
+    public string Job_Address { get; set; }
     public string Job_City { get; set; }
+    public string Job_Postal {get; set;}
+    public string Job_Stage { get; set; }
+    
 }
--- QxAPI/Models/JobContext.cs
+++ QxAPI/Models/JobContext.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
 namespace QxAPI.Models {
     public class JobContext : DbContext{
@@ -11,11 +10,13 @@ namespace QxAPI.Models {
         protected override void OnModelCreating(ModelBuilder modelBuilder){
             modelBuilder.Entity<Job>(eb => {
                 eb.HasNoKey();
-                eb.ToView("qryRpt_AllSchedItems1");
+                eb.ToView("QXInfo");
                 eb.Property(v => v.Job_Date).HasColumnName("Job_Date");
-                eb.Property(v => v.Job_Site).HasColumnName("JobSiteFileName");
-                eb.Property(v => v.Job_Stage).HasColumnName("Job_Description");
-                eb.Property(v => v.Job_City).HasColumnName("JobSiteLine2");
+                eb.Property(v => v.Job_Builder).HasColumnName("Job_Builder");
+                eb.Property(v => v.Job_Address).HasColumnName("Job_Address");
+                eb.Property(v => v.Job_City).HasColumnName("Job_City");
+                eb.Property(v => v.Job_Postal).HasColumnName("Job_Postal");
+                eb.Property(v => v.Job_Stage).HasColumnName("Job_Descrip");
             });
         }
     }
