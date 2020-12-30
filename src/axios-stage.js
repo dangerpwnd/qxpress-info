ddf8fadda736159b9bb98a2dc9da33134d6d5c0a
Dangerpwnd <reric2012@gmail.com>
2020-12-30 14:20:28 -0600

    For separate env instances

--- src/axios-stage.js
+++ src/axios-stage.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 const instance = axios.create({
     // FOR DEV
-    //baseURL: 'http://localhost:5000/api/Jobs'
-    baseURL: '/api/Jobs'
+    baseURL: 'http://localhost:5000/api/Jobs'
+    // FOR PROD
+    // baseURL: '/api/Jobs'
 });
 
 export default instance;
