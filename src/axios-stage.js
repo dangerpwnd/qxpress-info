799b5e705ceffdbf433702fd5ddde995a27e2ebf
Dangerpwnd <reric2012@gmail.com>
2021-04-06 13:06:20 -0500

    Added dev/prod axios instances

--- src/axios-stage.js
+++ src/axios-stage.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const instance = axios.create({
     // FOR DEV
-    baseURL: 'http://localhost:5000/api/Jobs'
+    // baseURL: 'http://localhost:5000/api/Jobs'
     // FOR PROD
-    // baseURL: '/api/Jobs'
+    baseURL: '/api/Jobs'
 });
 
 export default instance;
