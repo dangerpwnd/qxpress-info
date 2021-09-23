3561cff43e12afd64e18b3126caa32b863901d5c
dangerpwnd <reric2012@gmail.com>
2021-09-23 14:59:30 -0500

    Create Stages API specific stage query

---
+++ pages/api/stages/[stage].js
@@ -0,0 +1,31 @@
+import config from '../../../knexconf'
+
+export default function getJobsByStage (req, res) {
+    if(req.method !== 'GET'){
+        res.status(500).json({message: 'Only GET requests accepted'})
+    }
+
+    const {stage} = req.query;
+    const knex = require('knex')(config);
+
+    knex({qx: 'QXInfo'})
+        .where('qx.Job_Descrip', stage)
+        .select({
+            jobDate: 'qx.Job_Date',
+            jobBuilder: 'qx.Job_Builder',
+            jobAddress: 'qx.Job_Address',
+            jobCity: 'qx.Job_City',
+            jobPostal: 'qx.Job_Postal',
+            jobStage: 'qx.Job_Descrip',
+            jobNotes: 'qx.Job_Notes'
+            // jobColor: 'qx.Job_Color'
+        })
+        .then((resp) => {
+            return resp;
+        })
+        .catch((err) => {
+            return err;
+        });
+
+
+}
