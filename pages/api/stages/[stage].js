2711c2a113ae32cd1d4fa3e4f2b69f1fda7252b8
Dangerpwnd <reric2012@gmail.com>
2021-10-05 12:03:01 -0500

    Use next-connect, create GET response for stages

--- pages/api/stages/[stage].js
+++ pages/api/stages/[stage].js
@@ -1,31 +1,35 @@
 import config from '../../../knexconf'
+import nc from 'next-connect'
 
-export default function getJobsByStage (req, res) {
-    if(req.method !== 'GET'){
-        res.status(500).json({message: 'Only GET requests accepted'})
-    }
+const knex = require('knex')(config);
 
-    const {stage} = req.query;
-    const knex = require('knex')(config);
+const getJobsByStage =  nc()
+    .get((req, res) => {
+        const { stage, start, end } = req.query;
 
-    knex({qx: 'QXInfo'})
-        .where('qx.Job_Descrip', stage)
-        .select({
-            jobDate: 'qx.Job_Date',
-            jobBuilder: 'qx.Job_Builder',
-            jobAddress: 'qx.Job_Address',
-            jobCity: 'qx.Job_City',
-            jobPostal: 'qx.Job_Postal',
-            jobStage: 'qx.Job_Descrip',
-            jobNotes: 'qx.Job_Notes'
-            // jobColor: 'qx.Job_Color'
-        })
-        .then((resp) => {
-            return resp;
-        })
-        .catch((err) => {
-            return err;
-        });
+        const knexQuery = () => {
+            knex({qx: 'QXInfo'})
+            .where('qx.Job_Descrip', stage)
+            .whereBetween('qx.Job_Date', [start, end])
+            .select({
+                jobDate: 'qx.Job_Date',
+                jobBuilder: 'qx.Job_Builder',
+                jobAddress: 'qx.Job_Address',
+                jobCity: 'qx.Job_City',
+                jobPostal: 'qx.Job_Postal',
+                jobStage: 'qx.Job_Descrip',
+                jobNotes: 'qx.Job_Notes',
+                jobColor: 'qx.Job_Color',
+            })
+            .then(resp => {
+                res.send(resp)
+            })
+            .catch(err => {
+                res.json(err)
+            })
+        }
+        knexQuery();
+        }  
+    )
 
-
-}
+export default getJobsByStage
--- pages/api/stages/index.js
+++ pages/api/stages/index.js
@@ -1,29 +1,35 @@
 import config from '../../../knexconf'
+import nc from 'next-connect'
 
-export default function getJobsByStage (req, res) {
-    if(req.method !== 'GET'){
-        res.status(500).json({message: 'Only GET requests accepted'})
-    }
+const knex = require('knex')(config);
+const today = new Date().toLocaleDateString();
+const fullWeek =  new Date();
+fullWeek.setDate(fullWeek.getDate() + 7);
 
-    const knex = require('knex')(config);
+const getJobsByStages =  nc()
+    .get((req, res) => {
+        const knexQuery = () => {
+            knex({qx: 'QXInfo'})
+            .whereBetween('qx.Job_Date', [today, fullWeek])
+            .select({
+                jobDate: 'qx.Job_Date',
+                jobBuilder: 'qx.Job_Builder',
+                jobAddress: 'qx.Job_Address',
+                jobCity: 'qx.Job_City',
+                jobPostal: 'qx.Job_Postal',
+                jobStage: 'qx.Job_Descrip',
+                jobNotes: 'qx.Job_Notes',
+                jobColor: 'qx.Job_Color',
+            })
+            .then(resp => {
+                res.send(resp)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }
+        knexQuery();
+        }  
+    )
 
-    knex({qx: 'QXInfo'})
-        .select({
-            jobDate: 'qx.Job_Date',
-            jobBuilder: 'qx.Job_Builder',
-            jobAddress: 'qx.Job_Address',
-            jobCity: 'qx.Job_City',
-            jobPostal: 'qx.Job_Postal',
-            jobStage: 'qx.Job_Descrip',
-            jobNotes: 'qx.Job_Notes'
-            // jobColor: 'qx.Job_Color'
-        })
-        .then((resp) => {
-            return resp;
-        })
-        .catch((err) => {
-            return err;
-        });
-
-
-}
+export default getJobsByStages
