595f62b84dd0c225b47746b2ae701817b8c77e8f
dangerpwnd <reric2012@gmail.com>
2021-10-14 16:32:08 -0500

    Create LinkNav component

---
+++ Components/LinkNav.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const LinkNav = () => {
+    return (
+        <nav className="w-full flex justify-evenly">
+        <Link href="/jobtypes"><a className="p-4 text-white bg-blue-500 active:bg-blue-700">By Job Types</a></Link>
+        <Link href="/techs"><a className="p-4 text-white bg-blue-500 active:bg-blue-700">By Techs</a></Link>
+      </nav>
+    )
+}
+
+export default LinkNav;
