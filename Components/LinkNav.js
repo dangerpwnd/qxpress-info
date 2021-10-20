d8516685f37f7c3726d0bc8467cd896558b0a4bd
dangerpwnd <reric2012@gmail.com>
2021-10-20 09:20:19 -0500

    Update LinkNav layout

--- Components/LinkNav.js
+++ Components/LinkNav.js
@@ -1,19 +1,27 @@
 import Link from 'next/link';
 
-const LinkNav = () => {
+const LinkNav = (props) => {
   return (
-    <nav className="w-full flex justify-evenly">
-      <Link href="/jobtypes">
-        <a className="p-4 text-white bg-blue-500 active:bg-blue-700">
-          By Job Types
-        </a>
-      </Link>
-      <Link href="/techs">
-        <a className="p-4 text-white bg-blue-500 active:bg-blue-700">
-          By Techs
-        </a>
-      </Link>
-    </nav>
+    <div className="grid grid-cols-3 h-36 items-center">
+      <nav className="w-full flex flex-col justify-evenly">
+        <Link href="/jobtypes">
+          <a className="p-4 text-white bg-blue-500 active:bg-blue-700">
+            By Job Types
+          </a>
+        </Link>
+        <Link href="/techs">
+          <a className="p-4 text-white bg-blue-500 active:bg-blue-700">
+            By Techs
+          </a>
+        </Link>
+      </nav>
+      <div>
+        <h1 className="text-center text-2xl">{props.heading}</h1>
+      </div>
+      <div>
+        <h1 className="text-center">What would you like here?</h1>
+      </div>
+    </div>
   );
 };
 
