3522a78cf79904a28c660f78fd2daad2a6ca18be
Dangerpwnd <reric2012@gmail.com>
2021-11-01 16:44:08 -0500

    Updated theme colors, extended to allow tailwind preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,12 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      'mute-blue': '#5B7887',
-      'bright-blue': '#00798F',
-      'mute-green': '#62875B',
-      'bright-green': '#1A8A0F',
-      'mute-brown': '#876A5B',
-      'mute-purp': '#805B87',
-      'main-purp': {
-        light: '#673888',
-        dark: '#4d1b7b',
+    extend: {
+      colors: {
+        'main-purp': '#72506B',
+        'mute-purp': '#8A5A68',
       },
     },
-    extend: {},
     variants: {},
     plugins: [],
   },
