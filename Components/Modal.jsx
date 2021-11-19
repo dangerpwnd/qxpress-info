42c265a60aefeff047f5df98cfca0d759c77b5f5
Eric Dangerstone <reric2012@gmail.com>
2021-11-19 14:40:08 -0600

    Style Modal, Map notes to p elements

--- Components/Modal.jsx
+++ Components/Modal.jsx
@@ -1,41 +1,35 @@
 import React from 'react';
 
-const Modal = ({ notes, show }) => {
-  console.log(show);
-  if (!show) {
-    return null;
-  }
+const Modal = ({ notes }) => {
   return (
     <div
-      class="fixed z-10 inset-0 overflow-y-auto"
+      className="fixed z-10 inset-0 overflow-y-auto"
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
     >
-      <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div
-          class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
           aria-hidden="true"
         ></div>
-        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-          <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-            <div class="sm:flex sm:items-start">
-              <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-16 sm:align-middle sm:max-w-lg sm:w-full">
+          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                 <h2 className="text-2xl text-black">Job Notes</h2>
-                <p className="text-xl"></p>
-                <div class="mt-2">
-                  <p class="text-sm text-gray-500">{notes}</p>
-                </div>
+                {notes.map((note) => {
+                  return (
+                    <p key={note} className="text-sm text-gray-500">
+                      {note}
+                    </p>
+                  );
+                })}
               </div>
             </div>
           </div>
-          <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-            <button
-              type="button"
-              class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-            >
-              Close
-            </button>
+          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <p className="text-black">Press ESC to exit</p>
           </div>
         </div>
       </div>
