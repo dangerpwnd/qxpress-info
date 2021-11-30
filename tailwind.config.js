126a35f0d7ea4218a8fce4f7094ebf55a96f4d04
Eric Dangerstone <reric2012@gmail.com>
2021-11-29 20:59:50 -0600

    Extend theme with additional colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,20 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './Components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      'main-purp': '#72506B',
-      'mute-purp': '#8A5A68',
-      'off-white': '#D4CAD5',
-      'teal-500': '#008080',
-      'orange-500': '#FFA500',
-      'yellow-500': '#FFFF00',
-      'grey-500': '#808080',
-      'green-500': '#22C55E',
-      'red-500': '#EF4444',
-      'pink-500': '#EC4899',
-      'orange-500': '#F97316',
+    extend: {
+      colors: {
+        'main-purp': '#72506B',
+        'mute-purp': '#8A5A68',
+        'off-white': '#D4CAD5',
+        teal: '#008080',
+        orange: '#FFA500',
+        yellow: '#FFFF00',
+        grey: '#808080',
+        green: '#22C55E',
+        red: '#EF4444',
+        pink: '#EC4899',
+        brown: '#964B00',
+      },
     },
     variants: {},
     plugins: [],
