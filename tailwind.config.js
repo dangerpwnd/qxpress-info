af83a0c4a6b315a3742280cd806d837dbf22ffec
Eric Dangerstone <reric2012@gmail.com>
2021-11-29 20:23:24 -0600

    Override default color for dynamic color expression

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,18 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './Components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        'main-purp': '#72506B',
-        'mute-purp': '#8A5A68',
-        'off-white': '#D4CAD5',
-        'teal-500': '#008080',
-        'orange-500': '#FFA500',
-        'yellow-500': '#FFFF00',
-        'grey-500': '#808080',
-      },
+    colors: {
+      'main-purp': '#72506B',
+      'mute-purp': '#8A5A68',
+      'off-white': '#D4CAD5',
+      'teal-500': '#008080',
+      'orange-500': '#FFA500',
+      'yellow-500': '#FFFF00',
+      'grey-500': '#808080',
+      'green-500': '#22C55E',
+      'red-500': '#EF4444',
+      'pink-500': '#EC4899',
+      'orange-500': '#F97316',
     },
     variants: {},
     plugins: [],
