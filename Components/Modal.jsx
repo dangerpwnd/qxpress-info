eae10a884471f315e96990ac14e6e936337b73b5
Eric Dangerstone <reric2012@gmail.com>
2021-11-29 21:00:17 -0600

    Remove shade on colors

--- Components/Modal.jsx
+++ Components/Modal.jsx
@@ -10,7 +10,7 @@ const Modal = ({ notes }) => {
     >
       <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div
-          className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity inner"
+          className="fixed inset-0 bg-gray bg-opacity-75 transition-opacity inner"
           aria-hidden="true"
         ></div>
         <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-16 sm:align-middle sm:max-w-lg sm:w-full">
@@ -20,7 +20,7 @@ const Modal = ({ notes }) => {
                 <h2 className="text-2xl text-black">Job Notes</h2>
                 {notes.map((note) => {
                   return (
-                    <p key={note} className="text-sm text-gray-500">
+                    <p key={note} className="text-sm text-gray">
                       {note}
                     </p>
                   );
