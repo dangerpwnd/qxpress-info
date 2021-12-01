9b3b926ab56c4b9089f8b6c735b31128584e6a86
Eric Dangerstone <reric2012@gmail.com>
2021-12-01 12:37:54 -0600

    Add DateFormat function to be applied to resp

--- pages/api/jobtypes/[jobtype].js
+++ pages/api/jobtypes/[jobtype].js
@@ -1,37 +1,42 @@
-import config from '../../../knexconf'
-import nc from 'next-connect'
+import config from '../../../knexconf';
+import nc from 'next-connect';
 
 const knex = require('knex')(config);
 
-const getJobsByJobType =  nc()
-    .get((req, res) => {
-        const { jobtype, start, end } = req.query;
+const handleDateFormat = (resp) => {
+  resp.forEach((col) => {
+    const newDate = new Date(col.jobDate).toISOString().split('T')[0];
+    col.jobDate = newDate;
+  });
+};
 
-        const knexQuery = () => {
-            knex({qx: 'QXInfo'})
-            .where('qx.Job_Descrip', 'like', `%${jobtype}%`)
-            .whereBetween('qx.Job_Date', [start, end])
-            .select({
-                jobDate: 'qx.Job_Date',
-                jobBuilder: 'qx.Job_Builder',
-                jobAddress: 'qx.Job_Address',
-                jobCity: 'qx.Job_City',
-                jobPostal: 'qx.Job_Postal',
-                jobStage: 'qx.Job_Descrip',
-                jobNotes: 'qx.Job_Notes',
-                jobColor: 'qx.Job_Color',
-                jobCrew: 'qx.Job_Crew',
-            })
-            .then(resp => {
-                console.log(resp)
-                res.send(resp)
-            })
-            .catch(err => {
-                res.json(err)
-            })
-        }
-        knexQuery();
-        }  
-    )
+const getJobsByJobType = nc().get((req, res) => {
+  const { jobtype, start, end } = req.query;
 
-export default getJobsByJobType
+  const knexQuery = () => {
+    knex({ qx: 'QXInfo' })
+      .where('qx.Job_Descrip', 'like', `%${jobtype}%`)
+      .whereBetween('qx.Job_Date', [start, end])
+      .select({
+        jobDate: 'qx.Job_Date',
+        jobBuilder: 'qx.Job_Builder',
+        jobAddress: 'qx.Job_Address',
+        jobCity: 'qx.Job_City',
+        jobPostal: 'qx.Job_Postal',
+        jobStage: 'qx.Job_Descrip',
+        jobNotes: 'qx.Job_Notes',
+        jobColor: 'qx.Job_Color',
+        jobCrew: 'qx.Job_Crew',
+      })
+      .then((resp) => {
+        handleDateFormat(resp);
+        res.send(resp);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  };
+  knexQuery();
+});
+
+export default getJobsByJobType;
--- pages/api/techs/[tech].js
+++ pages/api/techs/[tech].js
@@ -1,36 +1,42 @@
-import config from '../../../knexconf'
-import nc from 'next-connect'
+import config from '../../../knexconf';
+import nc from 'next-connect';
 
 const knex = require('knex')(config);
 
-const getJobsByTechs =  nc()
-    .get((req, res) => {
-        const { tech, start, end } = req.query;
+const handleDateFormat = (resp) => {
+  resp.forEach((col) => {
+    const newDate = new Date(col.jobDate).toISOString().split('T')[0];
+    col.jobDate = newDate;
+  });
+};
 
-        const knexQuery = () => {
-            knex({qx: 'QXInfo'})
-            .where('qx.Job_Crew', tech)
-            .whereBetween('qx.Job_Date', [start, end])
-            .select({
-                jobDate: 'qx.Job_Date',
-                jobBuilder: 'qx.Job_Builder',
-                jobAddress: 'qx.Job_Address',
-                jobCity: 'qx.Job_City',
-                jobPostal: 'qx.Job_Postal',
-                jobStage: 'qx.Job_Descrip',
-                jobNotes: 'qx.Job_Notes',
-                jobColor: 'qx.Job_Color',
-                jobCrew: 'qx.Job_Crew',
-            })
-            .then(resp => {
-                res.send(resp)
-            })
-            .catch(err => {
-                res.json(err)
-            })
-        }
-        knexQuery();
-        }  
-    )
+const getJobsByTechs = nc().get((req, res) => {
+  const { tech, start, end } = req.query;
 
-export default getJobsByTechs
+  const knexQuery = () => {
+    knex({ qx: 'QXInfo' })
+      .where('qx.Job_Crew', tech)
+      .whereBetween('qx.Job_Date', [start, end])
+      .select({
+        jobDate: 'qx.Job_Date',
+        jobBuilder: 'qx.Job_Builder',
+        jobAddress: 'qx.Job_Address',
+        jobCity: 'qx.Job_City',
+        jobPostal: 'qx.Job_Postal',
+        jobStage: 'qx.Job_Descrip',
+        jobNotes: 'qx.Job_Notes',
+        jobColor: 'qx.Job_Color',
+        jobCrew: 'qx.Job_Crew',
+      })
+      .then((resp) => {
+        handleDateFormat(resp);
+        res.send(resp);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  };
+  knexQuery();
+});
+
+export default getJobsByTechs;
