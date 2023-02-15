02be7dad70e2c9d7329eaad965707cba29255c1d
Eric Dangerstone <edangerstone@gmail.com>
2023-02-15 16:35:09 -0600

    Removed unneeded brackets

--- pages/api/jobtypes/[jobtype].js
+++ pages/api/jobtypes/[jobtype].js
@@ -12,32 +12,30 @@ const handleDateFormat = (dates) => {
 
 const getJobsByJobType = nc().get((req, res) => {
   const { jobtype, start, end } = req.query;
-  {
-    const knexQuery = () => {
-      knexUpdated({ qx: 'QXInfo' })
-        .where('qx.Job_Descrip', 'like', `%${jobtype}%`)
-        .whereBetween('qx.Job_Date', [start, end])
-        .select({
-          jobDate: 'qx.Job_Date',
-          jobBuilder: 'qx.Job_Builder',
-          jobAddress: 'qx.Job_Address',
-          jobCity: 'qx.Job_City',
-          jobPostal: 'qx.Job_Postal',
-          jobStage: 'qx.Job_Descrip',
-          jobNotes: 'qx.Job_Notes',
-          jobColor: 'qx.Job_Color',
-          jobCrew: 'qx.Job_Crew',
-        })
-        .then((resp) => {
-          handleDateFormat(resp);
-          res.send(resp);
-        })
-        .catch((err) => {
-          res.json(err);
-        });
-    };
-    knexQuery();
-  }
+  const knexQuery = () => {
+    knexUpdated({ qx: 'QXInfo' })
+      .where('qx.Job_Descrip', 'like', `%${jobtype}%`)
+      .whereBetween('qx.Job_Date', [start, end])
+      .select({
+        jobDate: 'qx.Job_Date',
+        jobBuilder: 'qx.Job_Builder',
+        jobAddress: 'qx.Job_Address',
+        jobCity: 'qx.Job_City',
+        jobPostal: 'qx.Job_Postal',
+        jobStage: 'qx.Job_Descrip',
+        jobNotes: 'qx.Job_Notes',
+        jobColor: 'qx.Job_Color',
+        jobCrew: 'qx.Job_Crew',
+      })
+      .then((resp) => {
+        handleDateFormat(resp);
+        res.send(resp);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  };
+  knexQuery();
 });
 
 export default getJobsByJobType;
