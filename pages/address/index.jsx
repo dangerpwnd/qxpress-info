cdfee0d022997d972ac60baa658c6d8e9fd64491
Eric Dangerstone <edangerstone@gmail.com>
2023-02-24 16:24:17 -0600

    Updated QxFormAddress Component

--- pages/address/index.jsx
+++ pages/address/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
-import LinkNav from '../../Components/LinkNav';
 
+import LinkNav from '../../Components/LinkNav';
+import QxFormAddress from '../../Components/QxForm/QxFormAddress';
 import ErrorBoundary from '../../Components/ErrorBoundary';
 
-const ByAddress = () => {
+const ByAddress = (address) => {
   return (
     <>
       <Head>
@@ -14,6 +15,7 @@ const ByAddress = () => {
       <main>
         <ErrorBoundary>
           <LinkNav className="h-1/3" heading="Qxpress Job Reports By Address" />
+          <QxFormAddress address={address} />
         </ErrorBoundary>
       </main>
     </>
