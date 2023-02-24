e34a27d5949d85b7f40bea8506da73454f03fa58
Eric Dangerstone <edangerstone@gmail.com>
2023-02-24 16:23:32 -0600

    Added InputHandler and dynamic link

--- Components/LinkNav.jsx
+++ Components/LinkNav.jsx
@@ -3,16 +3,24 @@ import Link from 'next/link';
 import { gsap } from 'gsap';
 
 const LinkNav = (props) => {
-  //States
+  // States
+
   const [addrData, setAddrData] = useState('');
 
-  //Animations
+  // Animations
+
   const onEnter = ({ currentTarget }) => {
     gsap.to(currentTarget, { x: 50 });
   };
 
   const onLeave = ({ currentTarget }) => {
     gsap.to(currentTarget, { x: 0 });
+  };
+
+  // Handlers
+
+  const inputChangeHandler = (e) => {
+    setAddrData(e.target.value);
   };
 
   return (
@@ -37,16 +45,20 @@ const LinkNav = (props) => {
         <h1 className="text-center text-3xl">{props.heading}</h1>
       </div>
       <div className="self-center">
-        <h1 className="text-center text-xl">Enter Job Address:</h1>
-        <form className="text-center">
-          <input type="text" name="address" />
-          <button
-            type="submit"
-            className="text-x1 text-white font-bold border-2 m-2 p-2 border-white rounded-lg bg-mute-purp hover:bg-white focus:bg-white hover:border-mute-purp focus:border-mute-purp hover:text-mute-purp focus:text-mute-purp"
-          >
-            Find Address
-          </button>
-        </form>
+        <h1 className="text-center text-xl pb-4">Enter Job Address:</h1>
+        <div className="text-center">
+          <input
+            type="text"
+            className="text-black"
+            name="address"
+            onChange={inputChangeHandler}
+          />
+          <Link href={`/address/${encodeURIComponent(addrData)}`}>
+            <a className="text-x1 text-white font-bold border-2 m-2 p-2 border-white rounded-lg bg-mute-purp hover:bg-white focus:bg-white hover:border-mute-purp focus:border-mute-purp hover:text-mute-purp focus:text-mute-purp">
+              Find Address
+            </a>
+          </Link>
+        </div>
       </div>
     </div>
   );
