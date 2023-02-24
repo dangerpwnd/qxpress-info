e1e012cf0a1fceb25612835ce8a5d8e65817112b
Eric Dangerstone <edangerstone@gmail.com>
2023-02-24 16:23:52 -0600

    Added API call and data handlers

--- Components/QxForm/QxFormAddress.js
+++ Components/QxForm/QxFormAddress.js
@@ -4,19 +4,55 @@ import axios from 'axios';
 
 import QxTable from '../QxTable/QxTable';
 
-const QxFormAddress = () => {
+const QxFormAddress = (address) => {
   // States
+
   const [jobData, setJobData] = useState([]);
-  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
-    if (jobData.length > 0) {
-      setLoading(false);
-    }
-  }, [jobData]);
+    axios
+      .get('/api/address/' + address)
+      .then((resp) => {
+        resp.data.length !== 0
+          ? handleSplitCol(resp.data)
+          : console.log('No data available');
+        resp.data.length !== 0
+          ? handleDateFormat(resp.data)
+          : console.log('No data available');
+      })
+      .catch(() => {
+        alert('Address API Not Available');
+      });
+  }, []);
 
   // Handlers
-  const inputChangeHandler = (e) => {
-    setQxForm({ ...qxForm });
+
+  // Logic to split job_Address into Address, Community columns
+
+  const handleSplitCol = (resp) => {
+    resp.forEach((col) => {
+      let addrProps = col.address.split(/[-]+/);
+      col.Address = addrProps[0];
+      col.Community = addrProps[1];
+    });
+    setJobData(resp);
   };
+
+  // Logic to change format of date
+
+  const handleDateFormat = (resp) => {
+    resp.forEach((col) => {
+      const newDate = new Date(col.jobDate).toISOString().split('T')[0];
+      col.jobDate = newDate;
+    });
+    setJobData(resp);
+  };
+
+  return (
+    <>
+      <QxTable formData={jobData} />
+    </>
+  );
 };
+
+export default QxFormAddress;
